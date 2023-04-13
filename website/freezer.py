cf01ce52b9098ce5d1c6370164ca6ccad8527c99
chkla <christopher.klamm@hotmail.de>
2023-04-13 21:09:47 +0200

    clean repo

--- website/freezer.py
+++
@@ -1,9 +0,0 @@
-from main import app
-from flask_frozen import Freezer
-
-app.config['FREEZER_RELATIVE_URLS'] = True
-freezer = Freezer(app)
-
-if __name__ == '__main__':
-    # freezer.run(debug=True)
-    freezer.freeze()
